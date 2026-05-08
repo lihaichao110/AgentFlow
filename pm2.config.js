f482ec22ef583f6f07bac36676ae0df801055056
li-hai-chao <lihaichao@kezaihui.com>
2026-05-08 20:30:06 +0800

    fix: 更新 pm2 配置文件中的脚本路径，从 dist/main.js 修改为 dist/src/main.js

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'agentflow',
-      script: 'dist/main.js', // NestJS 编译后的入口
+      script: 'dist/src/main.js', // NestJS 编译后的入口
       // instances: 'max', // 多核利用
       // exec_mode: 'cluster', // 集群模式
       autorestart: true, // 崩溃自动重启
